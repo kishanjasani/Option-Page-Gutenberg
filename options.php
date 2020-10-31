84b0eeff3b1b43922cd978ae472f2dcc459769de
Kishan Jasani <kishanjasani007@yahoo.in>
2020-10-31 09:29:36 +0530

    Register options page

--- options.php
+++ options.php
@@ -13,3 +13,28 @@
  * Text Domain:       options
  * Domain Path:       /languages
  */
+
+define( 'OPTION_PLUGIN_VERSION', '1.0.0' );
+
+function option_options_assets() {
+	wp_enqueue_script( 'option-plugin-script', plugins_url( '/', __FILE__ ) . 'build/build.js', array( 'wp-api', 'wp-i18n', 'wp-components', 'wp-element' ), OPTION_PLUGIN_VERSION, true );
+	wp_enqueue_style( 'codeinwp-awesome-plugin-style', plugins_url( '/', __FILE__ ) . 'build/build.css', array( 'wp-components' ), OPTION_PLUGIN_VERSION );
+}
+
+function option_add_option_menu() {
+	$page_hook_suffix = add_options_page(
+		__( 'Options Awesome Plugin', 'option' ),
+		__( 'Options Awesome Plugin', 'option' ),
+		'manage_options',
+		'awesome_option',
+		'option_menu_callback'
+	);
+
+	add_action( "admin_print_scripts-{$page_hook_suffix}", 'option_options_assets' );
+}
+
+add_action( 'admin_menu', 'option_add_option_menu' );
+
+function option_menu_callback() {
+	echo '<div id="option-awesome-plugin"></div>';
+}
