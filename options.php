0d8fe85f066a46c626305bcffd39738142df09da
Kishan Jasani <kishanjasani007@yahoo.in>
2020-10-31 08:52:17 +0530

    Add setting options headers

---
+++ options.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       Options Using Gutenberg
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the options api with gutenberg.
+ * Version:           1.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      7.2
+ * Author:            Kishan Jasani
+ * Author URI:        https://kishanjasani.wordpress.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       options
+ * Domain Path:       /languages
+ */
